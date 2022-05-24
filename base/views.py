8189c8f11a36f7629e4bb027783961647c1fcf3a
Tiago Maia Vieira <ctw02162@Administrators-MacBook-Pro-2.local>
2022-05-24 15:24:16 +0100

    add create, update and delete item

--- base/views.py
+++ base/views.py
@@ -1,16 +1,43 @@
+from ast import Delete
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+    
     template_name = 'base/task_list.html'
 
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
     template_name = 'base/task_detail.html'
 
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+    template_name = 'base/task_form.html'
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+    template_name = 'base/task_form.html'
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
+    template_name = 'base/task_confirm_delete.html'
