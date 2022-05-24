076d5fa7b8d209650c32dfcf8ecc1c384d08d448
Tiago Maia Vieira <ctw02162@Administrators-MacBook-Pro-2.local>
2022-05-24 12:41:14 +0100

    add detail view

--- base/views.py
+++ base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+    template_name = 'base/task_list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task_detail.html'
 
