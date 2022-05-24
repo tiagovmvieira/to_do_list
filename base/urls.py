e8c6578b30a7f29ebaa906a28630474155cd6fd9
Tiago Maia Vieira <tiagovmvieira@hotmail.com>
2022-05-24 15:32:47 +0100

    add create, update and delete item

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
     path('task-update/<int:pk>', TaskUpdate.as_view(), name = 'task-update'),
     path('task-delete/<int:pk>', TaskDelete.as_view(), name = 'task-delete')
+    path('mais_um_path')
 ]
